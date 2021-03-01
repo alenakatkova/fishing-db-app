f99442cece5898d772424cec2403236476f967b7
Alena Katkova <katkova.web@gmail.com>
2021-03-01 21:58:25 +0100

    design main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>База данных рыболовной станции</h1>
       </header>
+      <div className="content-container">
+        <section className="subsection">
+          <h2>Просмотр и редактирование</h2>
+          <ul className="menu">
+            <li><a className="menu-link">Флотилия</a></li>
+            <li><a className="menu-link">Сотрудники</a></li>
+            <li><a className="menu-link">Команды</a></li>
+            <li><a className="menu-link">Выходы на лов</a></li>
+          </ul>
+        </section>
+        <section className="subsection">
+          <h2>Формирование отчетов</h2>
+          <ul className="menu">
+            <li><a className="menu-link">Катера по году постройки</a></li>
+            <li><a className="menu-link">Катера по количеству выходов на лов</a></li>
+            <li><a className="menu-link">Катера по мощности</a></li>
+            <li><a className="menu-link">Сотрудники с наибольшим уловом</a></li>
+          </ul>
+        </section>
+
+      </div>
+      <footer>Разработано Катковой А.А.</footer>
     </div>
   );
 }
